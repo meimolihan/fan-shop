6b0377794aec75766760bb1bb5c7bc37d41eb0fd
meimolihan <meimolihan@gmail.com>
2026-09-18 17:28:54 +0800

    日常更新

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -199,9 +199,11 @@ info "写入发版备注 RELEASE_NOTES.md"
     printf '%s\n' "${MSG}"
     printf '\n'
   fi
-  printf 'Docker 镜像：\n'
-  printf '  - Docker Hub：docker pull mobufan/fan-shop:%s\n' "${TAG}"
-  printf '  - GHCR：docker pull ghcr.io/meimolihan/fan-shop:%s\n' "${TAG}"
+  printf 'docker pull mobufan/fan-shop:latest\n'
+  printf 'docker pull mobufan/fan-shop:%s\n' "${TAG}"
+  printf '\n'
+  printf 'docker pull ghcr.io/meimolihan/fan-shop:latest\n'
+  printf 'docker pull ghcr.io/meimolihan/fan-shop:%s\n' "${TAG}"
 } > RELEASE_NOTES.md
 
 # ===================== Git 提交 & Tag =====================
